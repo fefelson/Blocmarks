15b368f149ded86d4e9e902bff167140734e63a7
Edward Walsh <edwrdwalsh@gmail.com>
2016-06-01 21:59:12 -0400

    User Sign Up

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,27 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:notice] = "Welcome to Blocmarks!"
+      sign_in(@user)
+      redirect_to root_path
+
+    else
+      flash.now[:alert] = "There was an error creating your account.  Please try again."
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+end
