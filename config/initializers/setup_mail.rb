2b11e1eae113188e771d15218a07d09f46f75ae0
Xander Miller <xander.miller@gmail.com>
2016-06-07 20:12:37 +0000

    changes initializer to use mailgun

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,13 +1,16 @@
 if Rails.env.development? || Rails.env.production?
-  ActionMailer::Base.delivery_method = :smtp
+  # https://www.bloc.io/resources/mailgun-integration
   ActionMailer::Base.smtp_settings = {
-    address:        'smtp.sendgrid.net',
-    port:           '2525',
-    authentication: :plain,
-    user_name:      ENV["SENDGRID_USERNAME"],
-    password:       ENV["SENDGRID_PASSWORD"],
-    domain:         'heroku.com',
-    enable_startttls_auto: true
-
+    port:              587,
+    address:           'smtp.mailgun.org',
+    user_name:         ENV['MAILGUN_SMTP_LOGIN'],
+    password:          ENV['MAILGUN_SMTP_PASSWORD'],
+    domain:            'appurlhere.com',
+    authentication:    :plain,
+    content_type:      'text/html'
   }
+  ActionMailer::Base.delivery_method = :smtp
+  
+  # Makes debugging *way* easier.
+  ActionMailer::Base.raise_delivery_errors = true
 end
