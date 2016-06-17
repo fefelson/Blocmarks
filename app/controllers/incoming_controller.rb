c5be1262a2e71527fd07dc30e1277b4ee6cc5305
Edward Walsh <edwrdwalsh@gmail.com>
2016-06-16 20:13:42 -0400

    Mailgun

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -4,11 +4,9 @@ class IncomingController < ApplicationController
 
   def create
 
-    # binding.pry
-
     mail_user = params[:sender]
     @user = User.find_by email: mail_user
-    if !@user
+    unless @user
       @user = User.new(
         name: mail_user,
         email: mail_user,
@@ -19,22 +17,16 @@ class IncomingController < ApplicationController
     end
 
 
-
-
     mail_topic = params[:subject]
     @topic = Topic.find_by title: mail_topic
 
-    if !@topic
+    unless @topic
       @topic = Topic.new(
         title: mail_topic,
         user: @user
       )
-    @topic.save!
+      @topic.save!
     end
-
-
-
-
 
 
     mail_url = params["body-plain"]
@@ -45,20 +37,7 @@ class IncomingController < ApplicationController
 
     @bookmark.save
 
-    # You put the message-splitting and business
-    # magic here.
-     # Find the user by using params[:sender]
-     # Find the topic by using params[:subject]
-     # Assign the url to a variable after retreiving it from params["body-plain"]
-
-     # Check if user is nil, if so, create and save a new user
-
-     # Check if the topic is nil, if so, create and save a new topic
-
-     # Now that you're sure you have a valid user and topic, build and save a new bookmark
-
-
-    # Assuming all went well.
+    
     head 200
   end
 
